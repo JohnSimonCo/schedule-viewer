0d73da9c9719cd95bc679dd2da2a378a43505309
John Rapp <johnfarnes@hotmail.com>
2015-11-22 21:33:02 +0100

    Saved the worl

--- get_schedule.php
+++ get_schedule.php
@@ -27,7 +27,7 @@ if($response->code != 200) {
 
 //$response = file_get_contents("data.json");
 
-$schedules = $response->body;
+$schedules = json_decode($response, true);
 $schedule_found = false;
 
 mkdir("schedules/$week", 0777, true);
