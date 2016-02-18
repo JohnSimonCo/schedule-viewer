3ff08dd98f55e2b591b806e9ccd85edf7ec1287f
Simon Halvdansson <swesnowme@gmail.com>
2016-02-18 19:46:04 +0100

    Optimized fonts and cs

--- index.php
+++ index.php
@@ -17,8 +17,6 @@ include_once("includer.php");
     <meta http-equiv="expires" content="Sun, 01 Jan 2014 00:00:00 GMT"/>
     <meta http-equiv="pragma" content="no-cache" />
 
-    <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,700' rel='stylesheet' type='text/css'>
-    <link href="styles/style.css" rel="stylesheet" type="text/css">
     <?php
     include_style("styles/style.css");
     ?>
