ea76f1e07aec962650522edd081528c9a734e238
SweSnow <swesnowme@gmail.com>
2016-03-03 23:27:30 +0100

    Revert "Minor iPhone fixes"
    This reverts commit c5dd7eaa17c86f1f193e2a52f736b57187398942.

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ include_once("includer.php");
     <title>Värmdö Gymnasium Schema</title>
 
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, minimum-scale=1, user-scalable=no"">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, minimum-scale=1">
 
     <meta http-equiv="expires" content="Sun, 01 Jan 2014 00:00:00 GMT"/>
     <meta http-equiv="pragma" content="no-cache" />
@@ -64,99 +64,97 @@ include_once("includer.php");
 
 </script>
 
-<div id="wrapper">
+<div id="normal-layout">
 
-    <div id="normal-layout">
+    <header class="header">
+        <div class="panel">
 
-        <header class="header">
-            <div class="panel">
+            <div class="classPanelContainer noselect">
 
-                <div class="classPanelContainer noselect">
+                <div onclick="toggleDropdown()" id="className"><?php echo $initial["className"] ?></div>
 
-                    <div onclick="toggleDropdown()" id="className"><?php echo $initial["className"] ?></div>
+                <div id="dropdown-overlay-layout" onclick="toggleDropdown()"></div>
 
-                    <div id="dropdown-overlay-layout" onclick="toggleDropdown()"></div>
+                <div id="classDropdown">
 
-                    <div id="classDropdown">
+                    <?php
+                    foreach($metadata["classNames"] as $className) { ?>
+                        <li id="<?php echo $className ?>classDropdown" onclick="toggleDropdown(); changeClass('<?php echo $className ?>')" class="classListItem<?php if($className == $initial["className"]) echo ' selectedClass'?>">
+                            <?php echo $className ?>
+                        </li>
+                    <?php } ?>
 
-                        <?php
-                        foreach($metadata["classNames"] as $className) { ?>
-                            <li id="<?php echo $className ?>classDropdown" onclick="toggleDropdown(); changeClass('<?php echo $className ?>')" class="classListItem<?php if($className == $initial["className"]) echo ' selectedClass'?>">
-                                <?php echo $className ?>
-                            </li>
-                        <?php } ?>
-
-                    </div>
-                </div>
-
-                <div id="parseErrorMessage"></div>
-                <div id="parseErrorMessageSmall"></div>
-
-                <a id="aboutButton" href="about/">
-                    <svg viewBox="0 0 24 24">
-                        <path fill="#fff" d="M11,9H13V7H11M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M11,17H13V11H11V17Z" />
-                    </svg>
-                </a>
-
-                <div id="changeView" onclick="changeView()">
-                    <svg id="viewWeek" style="width:24px;height:24px" viewBox="0 0 24 24" <?php if($initial["view"] == "week") echo 'class="hidden"' ?>>
-                        <path fill="#fff" d="M16,5V18H21V5M4,18H9V5H4M10,18H15V5H10V18Z" />
-                    </svg>
-
-                    <svg id="viewDay" style="width:24px;height:24px" viewBox="0 0 24 24" <?php if($initial["view"] == "day") echo 'class="hidden"' ?>>
-                        <path fill="#fff" d="M20,3H3A1,1 0 0,0 2,4V10A1,1 0 0,0 3,11H20A1,1 0 0,0 21,10V4A1,1 0 0,0 20,3M20,13H3A1,1 0 0,0 2,14V20A1,1 0 0,0 3,21H20A1,1 0 0,0 21,20V14A1,1 0 0,0 20,13Z" />
-                    </svg>
-                </div>
-
-                <div class="weekSelect">
-                    <div onclick="weekBack()" class="weekSelectArrow noselect">
-                        <svg class="arrow" fill="#FFFFFF" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M15.41 16.09l-4.58-4.59 4.58-4.59L14 5.5l-6 6 6 6z"/>
-                            <path d="M0-.5h24v24H0z" fill="none"/>
-                        </svg>
-                    </div>
-                    <div id="weekSelectNow" class="noselect"><?php echo $initial["week"] ?></div>
-                    <div onclick="weekForward()" class="weekSelectArrow noselect">
-                        <svg class="arrow" fill="#FFFFFF"viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M8.59 16.34l4.58-4.59-4.58-4.59L10 5.75l6 6-6 6z"/>
-                            <path d="M0-.25h24v24H0z" fill="none"/>
-                        </svg>
-                    </div>
                 </div>
             </div>
 
-            <div id="dayHeader"></div>
-        </header>
+            <div id="parseErrorMessage"></div>
+            <div id="parseErrorMessageSmall"></div>
 
-        <div class="schedule-container">
-            <div id="dayChangeLeft" class="day-changer noselect" onclick="changeDay(0)">
-                <span class="day-changer-span">
-                    <svg id="dayChangeLeftIcon" class="schedule-day-arrow" fill="#fff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <a id="aboutButton" href="about/">
+                <svg viewBox="0 0 24 24">
+                    <path fill="#fff" d="M11,9H13V7H11M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M11,17H13V11H11V17Z" />
+                </svg>
+            </a>
+
+            <div id="changeView" onclick="changeView()">
+                <svg id="viewWeek" style="width:24px;height:24px" viewBox="0 0 24 24" <?php if($initial["view"] == "week") echo 'class="hidden"' ?>>
+                    <path fill="#fff" d="M16,5V18H21V5M4,18H9V5H4M10,18H15V5H10V18Z" />
+                </svg>
+
+                <svg id="viewDay" style="width:24px;height:24px" viewBox="0 0 24 24" <?php if($initial["view"] == "day") echo 'class="hidden"' ?>>
+                    <path fill="#fff" d="M20,3H3A1,1 0 0,0 2,4V10A1,1 0 0,0 3,11H20A1,1 0 0,0 21,10V4A1,1 0 0,0 20,3M20,13H3A1,1 0 0,0 2,14V20A1,1 0 0,0 3,21H20A1,1 0 0,0 21,20V14A1,1 0 0,0 20,13Z" />
+                </svg>
+            </div>
+
+            <div class="weekSelect">
+                <div onclick="weekBack()" class="weekSelectArrow noselect">
+                    <svg class="arrow" fill="#FFFFFF" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path d="M15.41 16.09l-4.58-4.59 4.58-4.59L14 5.5l-6 6 6 6z"/>
                         <path d="M0-.5h24v24H0z" fill="none"/>
                     </svg>
-                </span>
-            </div>
-            <div id="schedule">
-
-            </div>
-            <div id="dayChangeRight" class="day-changer noselect" onclick="changeDay(1)">
-                <span class="day-changer-span">
-                    <svg id="dayChangeRightIcon" class="schedule-day-arrow" fill="#fff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                </div>
+                <div id="weekSelectNow" class="noselect"><?php echo $initial["week"] ?></div>
+                <div onclick="weekForward()" class="weekSelectArrow noselect">
+                    <svg class="arrow" fill="#FFFFFF"viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path d="M8.59 16.34l4.58-4.59-4.58-4.59L10 5.75l6 6-6 6z"/>
                         <path d="M0-.25h24v24H0z" fill="none"/>
                     </svg>
-                </span>
+                </div>
             </div>
         </div>
-    </div>
-    <div id="overlay-layout">
-        <div id="modalMasterHolder">
-            <div class="full-center" "></div>
-            <div onclick="setModalVisibility(false)" id="modalContainer"></div>
+
+        <div id="dayHeader"></div>
+    </header>
+
+    <div class="schedule-container">
+        <div id="dayChangeLeft" class="day-changer noselect" onclick="changeDay(0)">
+            <span class="day-changer-span">
+                <svg id="dayChangeLeftIcon" class="schedule-day-arrow" fill="#fff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M15.41 16.09l-4.58-4.59 4.58-4.59L14 5.5l-6 6 6 6z"/>
+                    <path d="M0-.5h24v24H0z" fill="none"/>
+                </svg>
+            </span>
+        </div>
+        <div id="schedule">
+
+        </div>
+        <div id="dayChangeRight" class="day-changer noselect" onclick="changeDay(1)">
+            <span class="day-changer-span">
+                <svg id="dayChangeRightIcon" class="schedule-day-arrow" fill="#fff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M8.59 16.34l4.58-4.59-4.58-4.59L10 5.75l6 6-6 6z"/>
+                    <path d="M0-.25h24v24H0z" fill="none"/>
+                </svg>
+            </span>
         </div>
     </div>
 </div>
+<div id="overlay-layout">
+    <div id="modalMasterHolder">
+        <div class="full-center" "></div>
+        <div onclick="setModalVisibility(false)" id="modalContainer"></div>
+    </div>
+</div>
+
 <?php
 
 include_script("js/lib.js");
