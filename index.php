81fd5b2ec1d82b4cc533562a23f3f705a8e89668
John Rapp <johnfarnes@hotmail.com>
2016-05-24 17:40:58 +0200

    Added move domain and moved isMobile

--- index.php
+++ index.php
@@ -4,6 +4,9 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+include_once("is-mobile.php");
+// exports $isMobile
+
 $metadata = include("metadata.php");
 $initial = include("initial.php");
 
